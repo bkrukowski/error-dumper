d8f41ee8bad22cf6afa4d2ad940d5cf6aeeec72b
Bartłomiej Krukowski <bkrukowski@olx.pl>
2016-05-09 07:21:56 -0300

    refactoring - psr2

--- tests/Dumpers/CreateExceptionVariadic.php
+++ tests/Dumpers/CreateExceptionVariadic.php
@@ -6,15 +6,15 @@ class CreateExceptionVariadic
 {
     public static function createException()
     {
-        return self::_createException('1', 2, 3.1, new \stdClass(), null, array());
+        return self::finalCreateException('1', 2, 3.1, new \stdClass(), null, array());
     }
 
     public static function createException2()
     {
-        return self::_createException(1, 2);
+        return self::finalCreateException(1, 2);
     }
 
-    private static function _createException(...$params)
+    private static function finalCreateException(...$params)
     {
         $fn = function (...$params) {
             return new \Exception();
