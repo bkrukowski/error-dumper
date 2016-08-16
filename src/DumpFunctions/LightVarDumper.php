65598d743e42ad85ac99f53716f60b5c964e2eb4
Bartłomiej Krukowski <bkrukowski@olx.pl>
2016-08-16 19:16:31 +0200

    Smarter LightVarDumper

--- src/DumpFunctions/LightVarDumper.php
+++ src/DumpFunctions/LightVarDumper.php
@@ -11,11 +11,59 @@ class LightVarDumper implements DumpFunctionInterface
         ob_start();
         $previous = ini_get(static::XDEBUG_CONFIG_KEY);
         ini_set(static::XDEBUG_CONFIG_KEY, false);
-        var_dump($var);
+        var_dump($this->convertVar($var));
         ini_set(static::XDEBUG_CONFIG_KEY, $previous);
         $contents = ob_get_contents();
         ob_end_clean();
 
-        return '<pre>' . substr($contents, 0, 300) . '</pre>';
+        return '<pre>' . $contents . '</pre>';
+    }
+
+    private function convertVar($var)
+    {
+        switch (gettype($var)) {
+            case 'string':
+                return $this->convertString($var);
+
+            case 'array':
+                return $this->convertArray($var);
+
+            case 'object':
+                return $this->convertObject($var);
+
+            default:
+                return $var;
+        }
+    }
+
+    private function convertString($string)
+    {
+        if (strlen($string) > 300) {
+            $string = substr($string, 0, 300) . '...';
+        }
+        return $string;
+    }
+
+    private function convertArray(array $array)
+    {
+        $array = array_slice($array, 0, 10, true);
+        foreach ($array as &$item) {
+            $item = $this->convertVar($item);
+        }
+        return $array;
+    }
+
+    private function convertObject($object)
+    {
+        $reflection = new \ReflectionClass($object);
+        if ($reflection->hasMethod('__debugInfo'))
+        {
+            $method = $reflection->getMethod('__debugInfo');
+            if ($method->isPublic() && !$method->getParameters())
+            {
+                return call_user_func(array($object, '__debugInfo'));
+            }
+        }
+        return 'object(' . get_class($object) . ') {}';
     }
 }
